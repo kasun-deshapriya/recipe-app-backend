ad96614ec489592d1693b76b0f17408953f29d51
Kasun Deshapriya <132161149+kasun-deshapriya@users.noreply.github.com>
2025-03-31 01:07:23 +0530

    refactor CORS configuration for improved readability and maintainability

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(cookieParser());
 
-app.use(cors({
-  origin: ["http://localhost:3000", "https://recipeapp-omega-sandy.vercel.app"],
-  credentials: true, 
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://recipeapp-omega-sandy.vercel.app/",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
