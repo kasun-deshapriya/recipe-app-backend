3b779a76004a465f299fa2fe5e9734ad39106ef7
Kasun Deshapriya <132161149+kasun-deshapriya@users.noreply.github.com>
2025-03-30 21:46:36 +0530

    refactor database connection to use DATABASE_URL and enable SSL

--- db/db.js
+++ db/db.js
@@ -5,10 +5,12 @@ dotenv.config();
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  //   user: process.env.DB_USER,
+  //   host: process.env.DB_HOST,
+  //   database: process.env.DB_DATABASE,
+  //   password: process.env.DB_PASSWORD,
+  //   port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL, // Use environment variable
+  ssl: { rejectUnauthorized: false }, // Required for some cloud providers
 });
 console.log("pool", pool);
